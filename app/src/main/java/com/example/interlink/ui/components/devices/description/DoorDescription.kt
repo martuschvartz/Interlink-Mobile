b8ab715b77222be0fc405875170e2c500cfa7ca5
martuschvartz <schvartzmartina@gmail.com>
2024-06-20 00:25:41 -0300

    we keep pushing

--- app/src/main/java/com/example/interlink/ui/components/devices/description/DoorDescription.kt
+++ app/src/main/java/com/example/interlink/ui/components/devices/description/DoorDescription.kt
@@ -20,11 +20,10 @@ fun DoorDescription(
 ){
     // si en la pagina no cambia la descripcion es porque aca lo puse como variable "no ref"
 
-    var status = when(doorDevice.status){
-        Status.ON -> null
-        Status.OFF -> null
+    val status = when(doorDevice.status){
         Status.OPENED -> stringResource(id = R.string.opened)
         Status.CLOSED -> stringResource(id = R.string.closed)
+        else -> null
     }
 
     Row {
